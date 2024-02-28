2967dadfcde1870b8332e0043b2700c5c024b7d9
JunghwanRo <jhro@seas.upenn.edu>
2024-02-28 16:56:03 +0100

    HW6 working version except tb

--- src/ar_slam_base/ar_slam_base/mapping_kf_jro.py
+++ src/ar_slam_base/ar_slam_base/mapping_kf_jro.py
@@ -35,27 +35,9 @@ class MappingKF(RoverOdo):
         self.motor_state.copy(motor_state)
 
         # Implement Kalman prediction here
-
         # Compute the update in the body frame and the resulting uncertainty in the body frame
         DeltaX = iW * S
         DeltaP = np.zeros((3, 3))
-
-        # jacobians of f
-        # theta = self.X[2,0]
-        # nb_landmarks = len(self.idx)
-        # Jacf_xytheta = np.identity(3) + np.asmatrix([[0, 0, -sin(theta)*DeltaX[0,0] - cos(theta)*DeltaX[1,0]], [0, 0, cos(theta)*DeltaX[0,0] - sin(theta)*DeltaX[1,0]], [0, 0, 0]])
-        # A = np.block([[Jacf_xytheta, np.zeros(3,nb_landmarks)],
-        #             [np.zeros(nb_landmarks,3), np.identity(nb_landmarks)]])
-
-        # Qr = (encoder_precision**2)*np.identity(3)
-        # Q = np.block([[QR, np.zeros(3,nb_landmarks)], [np.zeros(nb_landmarks,3), np.zeros(nb_landmarks,nb_landmarks)]])
-
-        # displacement of the robot
-        # already done
-
-        # uncertainty of the robot
-        DeltaP = (encoder_precision**2) * np.identity(3)  # not sure about that though
-
         self.lock.release()
         return self.predict_delta(logger, DeltaX, DeltaP)
 
@@ -69,94 +51,112 @@ class MappingKF(RoverOdo):
         Rtheta = np.mat(
             [[cos(theta), -sin(theta), 0], [sin(theta), cos(theta), 0], [0, 0, 1]]
         )
+        # self.X[0:3,0] = ...
+        # self.P[0:3,0:3] = ...
 
-        # Q = diag([0.1**2, 0.1**2, (0.01)**2]) #covariance on the state X
+        RthetaDeltaX = Rtheta * DeltaX
 
-        self.X[0:3, 0] += Rtheta * DeltaX
-        self.P[0:3, 0:3] = Rtheta * DeltaP * np.transpose(Rtheta)
+        # Project the state ahead.
+        # Xk = f(Xk-1, Uk-1, 0)
+        self.X[0:3, 0] += RthetaDeltaX
+
+        # A = df/dx -> Jacobian
+        A = np.eye(len(self.X))
+        A[0:3, 0:3] = np.mat(
+            [[1, 0, -RthetaDeltaX[1, 0]], [0, 1, RthetaDeltaX[0, 0]], [0, 0, 1]]
+        )
+        # B = df/du -> Jacobian, need check...
+        B = np.zeros((len(self.X), len(DeltaX)))
+        # Q = Process noise covariance
+        Q = np.zeros((len(self.X), len(self.X)))
+        Q[0:3, 0:3] = np.mat(np.diag([0.01, 0.01, 0.0]))
+        # Qu = Diagonal matrix of 0.01, len(DeltaX) x len(DeltaX)
+        Qu = np.mat(np.diag([0.01] * len(DeltaX)))
+
+        # Project the error covariance ahead.
+        # Pk = Ak * Pk-1 * Ak^T + (B * Qu * B^T) + Q
+        self.P = A * self.P * A.T + B * Qu * B.T + Q
+        # TODO END
 
         self.lock.release()
         return (self.X, self.P)
 
     def update_ar(self, logger, Z, id, uncertainty):
-        # Z is a dictionary of id->vstack([x,y])
         self.lock.acquire()
-        # TODO
         logger.info("Update: Z=" + str(Z.T) + " X=" + str(self.X.T) + " Id=" + str(id))
         print("Update: Z=" + str(Z.T) + " X=" + str(self.X.T) + " Id=" + str(id))
         # Update the full state self.X and self.P based on landmark id
         # be careful that this might be the first time that id is observed
         # TODO
+        # self.X = ...
+        # self.P = ...
 
-        theta = self.X[2, 0]
-        c = cos(theta)
-        s = sin(theta)
-        R_minustheta = np.asmatrix([[c, s], [-s, c]])
+        if id not in self.idx:
+            # New landmark, update state and covariance
+            self.idx[id] = Z
+            self.X = np.vstack([self.X, Z])
+            self.P = np.pad(self.P, ((0, 2), (0, 2)), "constant")
+        else:
+            # Existing landmark, update state and covariance
 
-        if id not in self.idx.keys():  # if first time we see the landmark
-            Rtheta = np.transpose(R_minustheta)
-            landmark = self.X[0:2, 0] + Rtheta * Z
-            self.idx[id] = self.X.shape[0]
-            self.X = np.mat(
-                np.vstack([self.X, landmark])
-            )  # add the landmark to state X
-            self.P = np.block(
+            L = self.idx[id]
+            theta = self.X[2, 0]
+            R_minus_theta = np.mat(
+                [[cos(theta), sin(theta)], [-sin(theta), cos(theta)]]
+            )
+
+            # h_XL
+            h_XL = R_minus_theta * (L - self.X[0:2])
+            # H = dh/dx -> Jacobian
+            H = np.mat(
                 [
-                    [self.P, np.zeros((self.P.shape[0], 2))],
-                    [np.zeros((2, self.P.shape[0])), np.identity(2)],
+                    [-cos(theta), -sin(theta), h_XL[1, 0]],
+                    [sin(theta), -cos(theta), -h_XL[0, 0]],
                 ]
             )
-            # print(self.P)
-            # logger.info("update of P: ",self.P)
 
-        # observation function
-        # L = self.X[3+2*id:3+2*(id+1), 0]
-        Lid = self.idx[id]
-        L = self.X[Lid : Lid + 2, 0]
-        h = R_minustheta * (L - self.X[0:2, 0])
+            # K = kalman gain
+            # K = Pk * H^T * inv(H * Pk * H^T + R)
+            R = np.mat(np.diag([uncertainty**2, uncertainty**2]))
+            K = self.P * H.T * inv(H * self.P * H.T + R)
 
-        # compute its derivatives with respect to X and L
-        lx = L[0, 0]
-        ly = L[1, 0]
-        x = self.X[0, 0]
-        y = self.X[1, 0]
+            # Xk = Xk-1 + K (Zk - h(Xk-1))
+            self.X = self.X + K * (Z - h_XL)
+            self.X[2, 0] = self.normAngle(self.X[2, 0])
 
-        Jh_X = np.asmatrix(
-            [
-                [-c, -s, -(lx - x) * s + (ly - y) * c],
-                [s, -c, -(lx - x) * c - (ly - y) * s],
-            ]
-        )
+            # Pk = (I - K * H) * Pk
+            self.P = (np.eye(3) - K * H) * self.P
+        # TODO END
 
-        H = np.block([Jh_X, np.zeros((2, len(self.X) - 3))])
-        H[:, Lid : 2 + Lid] = R_minustheta
-
-        # compute kalman gain
-        K = self.P * np.transpose(H) * inv(H * self.P * np.transpose(H) + uncertainty)
-
-        self.X += K * (Z - h)
-        self.P = (np.identity(np.size(self.P, 0)) - K * H) * self.P
         self.lock.release()
         return (self.X, self.P)
 
     def update_compass(self, logger, Z, uncertainty):
         self.lock.acquire()
-        # TODO
         logger.info("Update: S=" + str(Z) + " X=" + str(self.X.T))
         # Update the full state self.X and self.P based on compass measurement
         # TODO
-        theta = self.X[2, 0]  # this is our prediction function h here
-        h = theta
+        # self.X = ...
+        # self.P = ...
 
-        # kalman gain K
-        # jacobian with respect to X
-        H = np.zeros((1, len(self.X)))
-        H[0, 2] = 1
+        # h_XTheta
+        h_XTheta = self.X[2, 0]
+        # H = dh/dx -> Jacobian
+        H = np.mat([[0, 0, 1]])
 
-        K = self.P * np.transpose(H) * inv(H * self.P * np.transpose(H) + uncertainty)
+        # K = kalman gain
+        # K = Pk * H^T * inv(H * Pk * H^T + R)
+        R = np.mat(np.diag([uncertainty**2]))
+        K = self.P * H.T * inv(H * self.P * H.T + R)
 
-        self.X += K * (Z - h)
-        self.P = (np.identity(np.size(self.P, 0)) - K * H) * self.P
+        # Xk = Xk-1 + K (Zk - h(Xk-1))
+        self.X = self.X + K * self.diffAngle(Z, h_XTheta)
+        self.X[2, 0] = self.normAngle(self.X[2, 0])
+
+        # Pk = (I - K * H) * Pk
+        self.P = (np.eye(3) - K * H) * self.P
+        # TODO END
+
         self.lock.release()
         return (self.X, self.P)
 
